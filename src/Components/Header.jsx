c8e62863589ca3f9f114b116e9e413c0e9cc5eed
nikil-20 <nikilgajankush@gmail.com>
2024-12-16 13:25:32 +0530

    2nd commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,29 @@
+
+
+function Header(){
+    return(
+        <div>
+            <h3>Hello from header </h3>
+    </div>
+
+    );
+}
+
+function Header2(){
+    return(
+        <div>
+            <h3>Hello from header2 </h3>
+        </div>
+    );
+}
+
+function Main(){
+    return(
+        <div>
+            <h3>Hello from main </h3>
+        </div>
+    );
+}
+
+export  {Header,Header2}
+export default Main;
